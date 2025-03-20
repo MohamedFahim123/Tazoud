a1554010ccfb6d18afb110bf6ae897b633e6dc1f
Abd Elrahman Essam Mohamed <bodyessam97a@gmail.com>
2025-03-20 09:40:14 +0200

    change layout

--- app/auth/utils/storeTokenOnServer.ts
+++ app/auth/utils/storeTokenOnServer.ts
@@ -3,11 +3,11 @@
 import { cookies } from "next/headers";
 
 export async function setServerCookie(name: string, value: string) {
-    const cookieStore = await cookies();
-    cookieStore.set(name, value, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        path: "/",
-        maxAge: 60 * 60 * 24 * 7,
-    });
-};
+  const cookieStore = await cookies();
+  cookieStore.set(name, value, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 7,
+  });
+}
